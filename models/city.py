3e0653a80a16d50379e6277646d02d0d245320e0
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-27 16:27:02 +0100

    fix syntax

--- models/city.py
+++ models/city.py
@@ -8,12 +8,12 @@ from os import getenv
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ Defines City class """
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         places = relationship("Place", backref="cities", cascade="all, delete")
     else:
         name = ""
