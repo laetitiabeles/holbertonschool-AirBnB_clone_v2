b6a4c0c9b46dc6a4da285cd8fc3045c0f7c2fb6b
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-27 16:33:12 +0100

    import models

--- models/state.py
+++ models/state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+import models
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String
@@ -10,6 +12,8 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
