2ae65de2d3739083cbe559ae42c0477804932bb1
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-29 14:36:34 +0100

    fix relationship

--- models/state.py
+++ models/state.py
@@ -17,7 +17,7 @@ class State(BaseModel, Base):
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade="all, delete", backref="state")
+        # cities = relationship('City', cascade="all, delete", backref="state")
     else:
         name = ""
 
