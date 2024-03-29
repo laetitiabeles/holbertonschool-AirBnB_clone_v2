d45645c580fddf2a3e58b7ade683332fabb2e0f8
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-29 16:34:51 +0100

    add ORM and relationships for Place

--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 
 import models
 from models.base_model import BaseModel, Base
-from models.city import City
+import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -16,8 +16,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        # cities = relationship('City', cascade="all, delete", backref="state")
+        cities = relationship('City', cascade="all, delete", backref="state")
     else:
         name = ""
 
@@ -25,7 +24,7 @@ class State(BaseModel, Base):
     def cities(self):
         """ Returns list of cities associated with state """
         cities_list = []
-        for city in models.storage.all(City).values():
+        for city in models.storage.all("City").values():
             if self.id == city.state_id:
                 cities_list.append(city)
         return cities_list
