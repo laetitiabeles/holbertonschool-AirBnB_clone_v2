513359296aed56e72435383e09706bdd090c1e9a
laetitiabeles <laetitiabeles9@gmail.com>
2024-03-29 17:11:31 +0100

    fix place

--- models/place.py
+++ models/place.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
-from os import getenv
+
 import models
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from models.review import Review
+from models.amenity import Amenity
+from sqlalchemy import Column, String, Float, Integer, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from os import getenv
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity_table = Table("place_amenity",
+                                Base.metadata,
+                                Column(
+                                    "place_id",
+                                    String(60),
+                                    ForeignKey("places.id"),
+                                    primary_key=True,
+                                    nullable=False
+                                    ),
+                                Column(
+                                    "amenity_id",
+                                    String(60),
+                                    ForeignKey("amenities.id"),
+                                    primary_key=True,
+                                    nullable=False
+                                    )
+                                )
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = "places"
+    """ Defines Place class """
+    __tablename__ = 'places'
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -26,6 +47,7 @@ class Place(BaseModel, Base):
 
         reviews = relationship("Review", backref="place",
                                cascade="all, delete", passive_deletes=True)
+
         amenities = relationship("Amenity", secondary="place_amenity",
                                  viewonly=False, backref="place_amenity")
     else:
@@ -52,3 +74,21 @@ class Place(BaseModel, Base):
                     list_reviews.append(value)
 
             return list_reviews
+
+        @property
+        def amenities(self):
+            """ Return Amenities list """
+            list_amenities = []
+
+            for value in models.storage.all(Amenity).values():
+                if value.place_id == self.id:
+                    list_amenities.append(value)
+
+            return list_amenities
+
+        @amenities.setter
+        def amenities(self, cls):
+            """ Amenity id """
+            if not isinstance(cls, Amenity):
+                return
+            self.amenity_ids.append(cls.id)
